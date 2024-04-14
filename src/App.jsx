a9a4d1c6e4e4d823f2d12cddfd23482b55062642
Akhilesh Sharma <akhilergo@gmail.com>
2024-04-15 05:17:50 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header/Header'
 import { Intro } from './components/intro/Intro'
 import { ExperienceBrands } from './components/ExperienceBrands/ExperienceBrands'
 import { ExperienceInfo } from './components/ExperienceInfo/ExperienceInfo'
@@ -17,6 +16,7 @@ function App() {
 
   return (
     <div className='main-container'>
+      <Header/>
       <Intro/>
       <ExperienceBrands/>
       <ExperienceInfo/>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+import linkedin from "../../assets/linkedin.png"
+import behance from "../../assets/behance.png"
+import email from "../../assets/email.png"
+import hamb from "../../assets/hamb.png"
+import "./Header.css"
+
+export const Header = () => {
+    return (
+        <div className='header-container'>
+            <div>
+                <img src={hamb} />
+            </div>
+            <div className='logo-container'>
+                <img id="logo" src={logo} />
+            </div>
+            <div className='header-links'>
+                <div>
+                    <img src={linkedin} />
+                </div>
+                <div>
+                    <img src={behance} />
+                </div>
+                <div>
+
+                    <img src={email} />
+                </div>
+            </div>
+        </div>
+    )
+}
